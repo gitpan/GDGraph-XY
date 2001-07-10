ed5c365012cda8f1cdc1b8f5569bbf381539a2d6
George A. Fitch III <gaf3@gaf3.com>
2001-07-10 03:40:39 +0000

    Import of GAFFER/GDGraph-XY-0.90 from CPAN.
    gitpan-cpan-distribution: GDGraph-XY
    gitpan-cpan-version:      0.90
    gitpan-cpan-path:         GAFFER/GDGraph-XY-0.90.tar.gz
    gitpan-cpan-author:       GAFFER
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,59 @@
+use GD::Graph::xylines;
+use GD::Graph::xypoints;
+use GD::Graph::xylinespoints;
+
+@data = ( 
+	[7, -6, 4, 3,4,5,6,7,7,7,7],
+	[ -1, 2, 3, 4,4,5,6,7,-1,-2,-6]
+	);
+	
+$my_graph = new GD::Graph::xylines(600,400);
+
+$my_graph->set(
+	title		=> 'I am the Walrus',
+	x_label	=> 'Monkey',
+	y_label	=> 'Donkey'
+	);
+
+  my $gd = $my_graph->plot(\@data);
+
+  open(IMG, '>xylines.png') or die $!;
+  binmode IMG;
+  print IMG $gd->png;
+
+
+@data = ( 
+	[7, -6, 4, 3,4,5,6,7,7,7,7],
+	[ -1, 2, 3, 4,4,5,6,7,-1,-2,-6]
+	);
+	
+$my_graph = new GD::Graph::xypoints(600,400);
+
+$my_graph->set(
+	title		=> 'I am the Walrus',
+	x_label	=> 'Monkey',
+	y_label	=> 'Donkey'
+	);
+
+  my $gd = $my_graph->plot(\@data);
+
+  open(IMG, '>xypoints.png') or die $!;
+  binmode IMG;
+  print IMG $gd->png;
+
+
+$my_graph = new GD::Graph::xylinespoints(600,400);
+
+$my_graph->set(
+	title		=> 'I am the Walrus',
+	x_label	=> 'Monkey',
+	y_label	=> 'Donkey'
+	);
+
+  my $gd = $my_graph->plot(\@data);
+
+  open(IMG, '>xylinespoints.png') or die $!;
+  binmode IMG;
+  print IMG $gd->png;
+
+
